1afdfcceee25cf44114e55a23810d67c41dc43ee
mh53182 <marie.i.077145@gmail.com>
2023-10-18 07:32:59 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+  get 'homes/about' => "homes#about",as: 'about'
+  devise_for :users
+  # ↑deviseを使用する際、URLに"users"を含むよ、の意
+
+  resources :books, only: [:create, :index, :show, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
